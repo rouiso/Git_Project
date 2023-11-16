e3e0d2c393273610a5d7da48f89bf2ac98b67a48
ahmedbenromdhane1 <benromdhane.ahmed.1@esprit.tn>
2023-11-16 22:05:45 +0100

    ahmeds commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Entrée with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Person person = new Person("John Doe", 25);
+        Book book1 = new Book("The Catcher in the Rye", "J.D. Salinger");
+        Book book2 = new Book("To Kill a Mockingbird", "Harper Lee");
 
-        // Press Maj+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        person.displayInfo();
 
-            // Press Maj+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Library library = new Library();
+        library.addBook(book1);
+        library.addBook(book2);
+
+        library.displayBooks();
     }
 }
